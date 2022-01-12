145ee765a4cb80816a08a70e4313af65ffc43beb
larryworm1127 <shilerong@gmail.com>
2022-01-11 23:25:43 -0500

    finished basic web api

--- api/models.py
+++ api/models.py
@@ -4,17 +4,20 @@ from django.db import models
 class Tag(models.Model):
     name = models.TextField(primary_key=True, max_length=50)
 
+    def __str__(self):
+        return f'Name={self.name}'
+
 
 class Image(models.Model):
-    image_id = models.IntegerField(primary_key=True, auto_created=True)
     description = models.TextField(max_length=200)
     is_public = models.BooleanField()
     url = models.TextField()
-    tag = models.ManyToManyField(
-        Tag,
-        through='ImageTags',
-        through_fields=('image', 'tag')
-    )
+
+    def __str__(self):
+        return f'ID={self.pk}, ' \
+               f'Desc={self.description}, ' \
+               f'Public={self.is_public}, ' \
+               f'URL={self.url}'
 
 
 class ImageTags(models.Model):
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Any, Dict
+from typing import List, Any, Dict
 
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics, status
@@ -13,19 +13,31 @@ from api.serializer import ImageSerializer, TagSerializer
 def add_image(request):
     image = ImageSerializer(data=request.data)
     tags = get_tags(request.data['tags'])
-    if image.is_valid() and all(map(lambda item: item['is_valid'], tags)):
-        image.save()
+    if image.is_valid() and all([tag['is_valid'] for tag in tags]):
+        image_inst = image.save()
         for tag in tags:
             if tag['is_new']:
-                tag['tag'].save()
-            image_tag = ImageTags.objects.create(tag=tag['tag'], image=image)
+                tag['tag'] = tag['tag'].save()
+            image_tag = ImageTags.objects.create(image=image_inst, tag=tag['tag'])
             image_tag.save()
-        return Response(image.data, status=status.HTTP_201_CREATED)
-    print(image.errors, [tag['tag'].errors for tag in tags if tag['is_new']])
-    return Response(image.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        new_data = [tag['tag'].data for tag in tags if tag['is_new']] + [image.data]
+        return Response(new_data, status=status.HTTP_201_CREATED)
+
+    errors = [tag['tag'].error for tag in tags if tag['is_new']] + [image.errors]
+    return Response(errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def get_tags(tags) -> List[Dict[str, Any]]:
+    """
+    Determine whether the input tags exist or not.
+
+    If tag does not exist, then we create new one. Otherwise, we
+    assign the existing tag object to the image.
+
+    :param tags: list of tag names associated with the new image
+    :return: a dictionary containing tag object, valid status, and new tag status.
+    """
     result = []
     for tag in tags:
         try:
